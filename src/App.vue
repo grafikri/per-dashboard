0e8f9eaf959f7abcb3e3e196e8805da49981d144
Serhan Coşkun <grafikri@gmail.com>
2021-04-17 12:58:42 +0300

    feat: adjust layout component and router structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view name="layout">
+      <router-view/>
+    </router-view>
   </div>
 </template>
---
+++ src/components/MainLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container mx-auto px-5 md:px-0">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+};
+</script>
