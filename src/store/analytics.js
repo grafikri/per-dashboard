b9373f28580d67a33f9b8732a33594003a60b429
Serhan Coşkun <grafikri@gmail.com>
2021-04-17 15:50:56 +0300

    feat: produce store which keep and manage analytics data

---
+++ src/store/analytics.js
@@ -0,0 +1,83 @@
+// @ts-check
+import chartApi from '../structure/chartApi';
+import { AnalyticDataType } from '../structure/enums';
+
+/**
+ * @typedef {{timestamp: number, value: number}} Data
+ */
+
+/**
+ * @typedef {{type: import('../structure/enums').AnalyticDataType, data: Data[]}} Analytic
+ */
+
+/**
+ * @typedef {{list: Analytic[]}} State
+ */
+
+/**
+ * @type {State}
+ */
+const state = {
+  list: [
+
+  ],
+};
+
+const getters = {
+  /**
+   *
+   * @param {State} mState
+   * @returns {Data[]}
+   */
+  ttfb: (mState) => mState.list.find((item) => item.type === AnalyticDataType.TTFB)?.data ?? [],
+  fcb: (mState) => mState.list.find((item) => item.type === AnalyticDataType.FCB)?.data ?? [],
+};
+
+const mutations = {
+  /**
+   *
+   * @param {State} state
+   * @param {Analytic[]} payload
+  */
+  // eslint-disable-next-line no-shadow
+  setList(state, payload) {
+    state.list = payload;
+  },
+};
+
+const actions = {
+  async fetchData({ commit }) {
+    // @ts-ignore
+    const response = Object.values(await chartApi.$get('/analytics'));
+
+    /**
+     * @type {Analytic[]}
+     */
+    const payload = [
+      {
+        type: AnalyticDataType.TTFB,
+        data: response.map((item) => ({
+          timestamp: item.date,
+          value: item.ttfb,
+        })),
+      },
+      {
+        type: AnalyticDataType.FCB,
+        data: response.map((item) => ({
+          timestamp: item.date,
+          value: item.domLoading,
+        })),
+      },
+    ];
+
+    commit('setList', payload);
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  getters,
+  mutations,
+};
