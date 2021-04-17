54f7d3bed48e944115b651219881f4c34b528213
Serhan Coşkun <grafikri@gmail.com>
2021-04-17 12:59:30 +0300

    feat: adjust grid system with responsive behaviour and dummy data for chart page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,46 @@
 <template>
-  <div class="home">
-    aa
+  <div class="">
+    <h1 class="py-5 text-2xl">Charts</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+      <div v-for="item in [1,2,3,4,5,6,7,8,9]" :key="item.id">
+        <div class="shadow p-3">
+          <basic-line-chart title="This is title" :dataSet="items" />
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import BasicLineChart from '../components/BasicLineChart.vue';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      items: [
+        {
+          label: 'Red',
+          value: 252,
+        },
+        {
+          label: 'Blue',
+          value: 19,
+        },
+        {
+          label: 'Yellow',
+          value: 3,
+        },
+        {
+          label: 'Green',
+          value: 5,
+        },
+      ],
+    };
+  },
   components: {
-
+    BasicLineChart,
   },
 };
 </script>
